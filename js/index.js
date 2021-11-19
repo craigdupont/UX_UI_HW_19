91ffb27db5fcfe6b7c3a8425343af9f377ea31c2
craigdupont <84634247+craigdupont@users.noreply.github.com>
2021-11-19 14:35:07 -0500

    Input scroll down to experiecne button
    placed a button to scroll down to the bottom of the page to the "experience" section

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("Your index.js file is loaded correctly!");
+
+$("#clickMe").on('click', function() {/*this is the function to scroll down*/
+    $("HTML, BODY").animate({
+        scrollTop: 3000 /*parameter for disctance down */
+    }, 1500);/*parameter for how fast scrolling down*/
+});
+
+
