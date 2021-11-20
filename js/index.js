bd6c3e3ad7cdb68695c44180222e465b197085a3
craigdupont <84634247+craigdupont@users.noreply.github.com>
2021-11-19 19:44:56 -0500

    Feedback Iterations
    made the drop down go all the way
    swapped the button in the hero with text and a down arrow
    aligned the skill section so it looks cleaner
    made the social media icons black/white to fit in with the black/white
    animated the nav bar underline so it looks cool

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ console.log("Your index.js file is loaded correctly!");
 
 $("#clickMe").on('click', function() {/*this is the function to scroll down*/
     $("HTML, BODY").animate({
-        scrollTop: 3000 /*parameter for disctance down */
+        scrollTop: 3500 /*parameter for disctance down */
     }, 1500);/*parameter for how fast scrolling down*/
 });
 
